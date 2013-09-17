7dbdc696ef63d1dd978eb5d518eaea80f326c06a
Konstantin Nikitin <stromsund@yandex-team.ru>
2013-09-17 19:59:24 +0400

    Added support for POST requests

--- main.py
+++ main.py
@@ -35,6 +35,9 @@ def send_data(environ):
         headers_list.append(('X-Auth-Fullname', auth_data.full_name))
         if 'CONTENT_TYPE' in environ:
             headers_list.append(('Content-Type', environ['CONTENT_TYPE']))
+        input_file = environ['wsgi.input'].read()
+        if input_file:
+            headers_list.append(('Content-Length', str(len(input_file))))
         header_template = '{name}: {value}\r\n'
         headers_str = ''.join(header_template.format(name=h[0], value=h[1])
                               for h in headers_list)
